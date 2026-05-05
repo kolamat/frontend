69b5d6443eb515b77b63d9362cd6713b7d78775d
kolamat <matanmidupe004@gmail.com>
2026-05-05 18:40:20 +0100

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-    <p className="text-center text-blue-800 text-7xl font-black">Hello, kazeem!</p>
+    <p className="">Hello, kazeem!</p>
     </div>
   )
 }
