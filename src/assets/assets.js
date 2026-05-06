8e0c32421fd068c34329918cd87c2a08bfd9b87b
kolamat <matanmidupe004@gmail.com>
2026-05-06 13:41:48 +0100

    modified:   src/assets/assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,24 +3,34 @@ import header_img from './header_img.png'
 import group_profiles from './group_profiles.png'
 import profile_pic from './profiles_pic.png'
 import contact_image from './contact_image.png'
-import about_image from './about_image'
-import logo from './logo.png'
-import dropdown_icon from './dropdown_icon.png'
-import menu_icon from './menu_icon.png'
+import about_image from './about_image.png'
+import logo from './logo.svg'
+import dropdown_icon from './dropdown_icon.svg'
+import menu_icon from './menu_icon.svg'
 import cross_icon from './cross_icon.png'
-import chats_icon from './chats_icon.png'
-import verified_icon from './verified_icon.png'
-import arrow_icon from './arrow_icon.png'
-import info_icon from './info_icon.png'
+import chats_icon from './chats_icon.svg'
+import verified_icon from './verified_icon.svg'
+import arrow_icon from './arrow_icon.svg'
+import info_icon from './info_icon.svg'
 import upload_icon from './upload_icon.png'
-import from
-import from
-import from
-import from
-import from
-import from
-import from
-import from
-import from
-import from
-import from
+import stripe_logo from './stripe_logo.png'
+import razorpay_logo from './razorpay_logo.png'
+import doc1 from './doc1.png'
+import doc2 from './doc2.png'
+import doc3 from './doc3.png'
+import doc4 from './doc4.png'
+import doc5 from './doc5.png'
+import doc6 from './doc6.png'
+import doc7 from './doc7.png'
+import doc8 from './doc8.png'
+import doc9 from './doc9.png'
+import doc10 from './doc10.png'
+import doc11 from './doc11.png'
+import doc12 from './doc12.png'
+import doc13 from './doc13.png'
+import Dermatologist from './Dermatologist.svg'
+import Gastroenterologist from './Gastroenterologist.svg'
+import General_physician from './General_physician.svg'
+import Gynocologist from './Gynocologist.svg'
+import Neurologist from './Neurologist.svg'
+import Pediatricians from './Pediatricians.svg'
