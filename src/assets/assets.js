94af682072f2aeafbe29cff98915cbed0706fbe5
kolamat <matanmidupe004@gmail.com>
2026-05-06 13:28:37 +0100

    modified:   src/assets/assets.js

---
+++ src/assets/assets.js
@@ -0,0 +1,26 @@
+import appointment_img from 'appointment_img.png'
+import header_img from './header_img.png'
+import group_profiles from './group_profiles.png'
+import profile_pic from './profiles_pic.png'
+import contact_image from './contact_image.png'
+import about_image from './about_image'
+import logo from './logo.png'
+import dropdown_icon from './dropdown_icon.png'
+import menu_icon from './menu_icon.png'
+import from
+import from
+import from
+import from
+import from
+import from
+import from
+import from
+import from
+import from
+import from
+import from
+import from
+import from
+import from
+import from
+import from
