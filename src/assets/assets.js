26d85e046eb2001ed3a52e0fb1c834de94e77b3d
kolamat <matanmidupe004@gmail.com>
2026-05-06 13:31:55 +0100

    modified:   src/assets/assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -7,12 +7,12 @@ import about_image from './about_image'
 import logo from './logo.png'
 import dropdown_icon from './dropdown_icon.png'
 import menu_icon from './menu_icon.png'
-import from
-import from
-import from
-import from
-import from
-import from
+import cross_icon from './cross_icon.png'
+import chats_icon from './chats_icon.png'
+import verified_icon from './verified_icon.png'
+import arrow_icon from './arrow_icon.png'
+import info_icon from './info_icon.png'
+import upload_icon from './upload_icon.png'
 import from
 import from
 import from
